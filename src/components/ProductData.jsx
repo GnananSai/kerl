426888391c293b9f19c61793e07eaa0ad016d134
tarunreddy127 <138034259+tarunreddy127@users.noreply.github.com>
2024-10-02 20:08:41 +0530

    Add files via upload

---
+++ src/components/ProductData.jsx
@@ -0,0 +1,37 @@
+const ProductData = [
+  {
+    id: 1,
+    name: 'Liquid Detergent',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus.  Pellentesque commodo lacus at sodales sodales. ',
+    images: [
+      '/images/image1.jpg',
+      '/images/image2.jpg',
+      '/images/image3.jpg'
+    ],
+    sizes: ['500 ml', '1 L', '2 L'],
+  },
+  {
+    id: 2,
+    name: 'Floor Cleaner',
+    description: 'Keep your floors shiny and clean with our eco-friendly floor cleaner.',
+    images: [
+      '/images/image1.jpg',
+      '/images/image2.jpg',
+      '/images/image3.jpg'
+    ],
+    sizes: ['1 L', '2 L', '5 L'],
+  },
+  {
+    id: 3,
+    name: 'Toilet Cleaner',
+    description: 'Powerful toilet cleaner for stain-free toilets.',
+    images: [
+     '/images/image1.jpg',
+      '/images/image2.jpg',
+      '/images/image3.jpg'
+    ],
+    sizes: ['500 ml', '1 L', '2 L'],
+  }
+];
+
+export default ProductData;
